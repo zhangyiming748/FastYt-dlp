84e292e9b954cbf7bd7bd7dc4bda7cf6bb536ecf
zen <zhangyiming748@gmail.com>
2024-05-30 18:43:39 +0800

    最高下载横屏1080P

--- batch.sh
+++ batch.sh
@@ -5,9 +5,10 @@ url_list_file="url_list.txt"
 
 # 按行读取网址列表文件，并在循环中打印每个网址
 while IFS= read -r url; do
-    if [[ $url == #* ]]; then
-        continue
-    fi
+    if [[ ${url:0:1} == "#" ]]; then
+    continue
+fi
+
     echo "$url"
-    yt-dlp --proxy 192.168.1.20:8889 $url
+    yt-dlp --proxy 192.168.1.20:8889 -f "bestvideo[height<=?1080]+bestaudio/best[height<=?1080]" $url
 done < "$url_list_file"
