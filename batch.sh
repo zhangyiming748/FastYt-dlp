e2e676a3f889d8a95ba342dce63cf0bc55c62f92
zen <zhangyiming748@gmail.com>
2024-06-07 19:40:51 +0800

    save

--- batch.sh
+++ batch.sh
@@ -2,7 +2,7 @@
 
 # 指定网址列表文件路径
 url_list_file="url_list.txt"
-prefix_dir="/mnt/c/Users/zen/Github/FastYt-dlp/afterwhisper joi"
+prefix_dir="/mnt/c/Users/zen/Github/FastYt-dlp/clip"
 # 按行读取网址列表文件，并在循环中打印每个网址
 while IFS= read -r url; do
     if [[ ${url:0:1} == "#" ]]; then
