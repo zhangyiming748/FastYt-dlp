8a700d09a109c1f840c7eb131d9214b52ef25677
zen <zhangyiming748@gmail.com>
2024-06-07 09:21:18 +0800

    手动设置下载目录

--- batch.sh
+++ batch.sh
@@ -2,7 +2,7 @@
 
 # 指定网址列表文件路径
 url_list_file="url_list.txt"
-
+prefix_dir="/mnt/c/Users/zen/Github/FastYt-dlp/afterwhisper joi"
 # 按行读取网址列表文件，并在循环中打印每个网址
 while IFS= read -r url; do
     if [[ ${url:0:1} == "#" ]]; then
@@ -10,5 +10,5 @@ while IFS= read -r url; do
 fi
 
     echo "$url"
-    yt-dlp --proxy 192.168.1.20:8889 -f "bestvideo[height<=?1080]+bestaudio/best[height<=?1080]" $url
+    yt-dlp --proxy 192.168.1.20:8889 -f "bestvideo[height<=?1080]+bestaudio/best[height<=?1080]" -o "${prefix_dir}/%(title)s.%(ext)s" $url --no-playlist
 done < "$url_list_file"
