d8edf74f5cdd9752122e8f1b3ba87bff06d546ed
zen <zhangyiming748@gmail.com>
2024-06-11 10:07:42 +0800

    自动根据文件名创建相应文件夹

--- batch.sh
+++ batch.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# 指定网址列表文件路径
-url_list_file="url_list.txt"
-prefix_dir="/mnt/c/Users/zen/Github/FastYt-dlp/clip"
-# 按行读取网址列表文件，并在循环中打印每个网址
-while IFS= read -r url; do
-    if [[ ${url:0:1} == "#" ]]; then
-    continue
-fi
+for file in *.list; do
+    dir_name=$(basename "$file" .list)
+    mkdir -p "$dir_name"
+    # 按行读取网址列表文件，并在循环中打印每个网址
+    while IFS= read -r url; do
+        if [[ ${url:0:1} == "#" ]]; then
+            continue
+        fi
 
-    echo "$url"
-    yt-dlp --proxy 192.168.1.20:8889 -f "bestvideo[height<=?1080]+bestaudio/best[height<=?1080]" -o "${prefix_dir}/%(title)s.%(ext)s" $url --no-playlist
-done < "$url_list_file"
+        echo "$url"
+        yt-dlp --proxy 192.168.1.20:8889 -f "bestvideo[height<=?1080]+bestaudio/best[height<=?1080]" -o "$dir_name/%(title)s.%(ext)s" $url --no-playlist
+    done <"$file"
+done
